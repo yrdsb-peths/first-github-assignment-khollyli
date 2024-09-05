318c78f8a1cf6e467b381ca7ff9743d0a1819758
khollyli <94080024+khollyli@users.noreply.github.com>
2024-09-05 13:08:37 -0400

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,15 @@
+import java.util.*;
+  
+public class Student
+{
+  public static void main()
+  {
+    Scanner scan = new Scanner (System.in);
+    System.out.println("Enter your first name: ");
+    String firstName = scan.nextLine();
+    System.out.println("Enter your last name: ");
+    String lastName = scan.nextLine();
+
+    System.out.println("Hello " + firstName + " " + lastName + "!");
+  }
+}
